2c26ce1928ecc4d90ebe8690cd771492b99a0f62
Thomas Gruppo-Oliver <blueskin90@gmail.com>
2021-10-06 18:48:30 +0200

    first pseudoping of an address

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -4,4 +4,20 @@
 #include "libftprintf.h"
 #include "libft.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+# define HANDLED_FLAGS "v"
+
+typedef struct		s_env
+{
+	char			*dest;
+}					t_env;
+
 # endif /* FT_PING_H */
--- srcs/ping/main.c
+++ srcs/ping/main.c
@@ -1,7 +1,56 @@
 #include "ft_ping.h"
 
-int			main(void)
+int		ft_usage(void)
 {
-	ft_printf("hello\n");
+	ft_printf("usage: ping [-%s] [-h sweepincrsize] host\n", HANDLED_FLAGS);
+	return (1);
+}
+
+int		ft_parsing(t_env* env, int ac, char **av)
+{
+	(void)ac;
+	env->dest = av[0];
+	return (1);
+}
+
+void		ft_print_addrinfo(struct addrinfo *ptr)
+{
+	printf("flags : %d\n", ptr->ai_flags);
+	printf("family : %d\n", ptr->ai_family);
+	printf("socktype : %d\n", ptr->ai_socktype);
+	printf("protocol : %d\n", ptr->ai_protocol);
+}
+
+void		ft_ping(t_env *env)
+{
+	struct addrinfo	hints;
+	struct addrinfo *res;
+	int				error;
+
+	(void)env;
+
+	res = NULL;
+	ft_bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = PF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	error = getaddrinfo(env->dest, "http", &hints, &res);
+	if (error)
+	{
+		printf("error %d: %s\n", error, gai_strerror(error));
+		return ;
+	}
+	ft_print_addrinfo(res);
+}
+
+int			main(int ac, char **av)
+{
+	t_env	env;
+
+	if (ac < 2)
+		return (ft_usage());
+	if (!ft_parsing(&env, ac - 1, av + 1))
+		return (1);
+	ft_ping(&env);	
 	return (0);
 }
